ccf3fdc27cddbd61e1fcb8bdaf1020292f8560c2
aimad <aimad.boulaadam02@gmail.com>
2025-12-02 00:16:17 +0100

    creating the book model

--- server/library_admin/models.py
+++ server/library_admin/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Book(models.Model):
+    title = models.CharField(max_length=200, help_text="Title of the book.")
+    author = models.CharField(max_length=100)
+    isbn = models.CharField(max_length=13, unique=True, help_text="13-Digit ISBN.")
+    is_available = models.BooleanField(default=True)
+    
+    def __str__(self):
+        return f"{self.title} by {self.author}"
